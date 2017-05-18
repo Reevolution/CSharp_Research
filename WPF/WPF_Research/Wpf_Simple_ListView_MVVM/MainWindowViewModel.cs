b3975192984acd6f0cf5684088a21363f7b48a9e
DESKTOP-LH38R6M\reevo <x5reunion@yandex.ru>
2017-05-18 21:53:05 +0300

    Added simple ListView MVVM example.

---
+++ WPF/WPF_Research/Wpf_Simple_ListView_MVVM/MainWindowViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf_Simple_ListView_MVVM
+{
+    /// <summary>
+    /// Simple MainWindow View Model
+    /// </summary>
+    public class MainWindowViewModel
+    {
+        public ObservableCollection<SimpleData> Items { get; set; }
+
+        public MainWindowViewModel()
+        {
+            Items = new ObservableCollection<SimpleData>()
+            {
+                new SimpleData(){Id= 23, FirstName = "Evgeny", SecondName = "Petrov"  },
+                new SimpleData(){Id= 1, FirstName = "Mixail", SecondName = "Volkov"  },
+                new SimpleData(){Id= 55, FirstName = "Dmitry", SecondName = "Serov"  },
+                new SimpleData(){Id= 255, FirstName = "Sergey", SecondName = "Smolov"  },
+                new SimpleData(){Id= 34, FirstName = "Alex", SecondName = "Ivanov" }
+            };
+        }
+    }
+}
---
+++ WPF/WPF_Research/Wpf_Simple_ListView_MVVM/SimpleData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf_Simple_ListView_MVVM
+{
+    /// <summary>
+    /// Simple data fro binding in ListView
+    /// </summary>
+    public class SimpleData
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+    }
+}
