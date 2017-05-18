20a7115b10140637c922908d5b5ed6b072bea944
DESKTOP-LH38R6M\reevo <x5reunion@yandex.ru>
2017-05-18 22:37:59 +0300

    Added simple event example. Event with simple action, second event send simple data.

---
+++ Events/Events_Research/Event_Simple_Example/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Event_Simple_Example
+{
+    // GitHub repo.
+    // https://github.com/Reevolution/CSharp_Research
+
+    /// <summary>
+    /// Simple example two events actions.
+    /// First simple event action just call action.
+    /// Second simple event action call action and send data from event.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SimpleData simpleData = new SimpleData();
+
+            simpleData.SimpleEvent_0 += SimpleAction_0;
+            simpleData.SimpleEvent_1 += SimpleAction_1;
+
+            Console.WriteLine("Second event counter");
+            simpleData.Counter_0();
+
+            Console.WriteLine("First event counter");
+            simpleData.Counter_1();
+        }
+
+        /// <summary>
+        /// Simple action no data.
+        /// </summary>
+        static void SimpleAction_0()
+        {
+            Console.WriteLine("Simple action after event.");
+        }
+
+        /// <summary>
+        /// Simple action get data from event.
+        /// </summary>
+        /// <param name="i"></param>
+        static void SimpleAction_1(int i)
+        {
+            Console.WriteLine($"Simple action after event. Data from event {i}");
+        }
+    }
+}
---
+++ Events/Events_Research/Event_Simple_Example/SimpleData.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Event_Simple_Example
+{
+    /// <summary>
+    /// Simple class describe two event actions.
+    /// </summary>
+    public class SimpleData
+    {
+        /// <summary>
+        /// First event.
+        /// </summary>
+        public event Action SimpleEvent_0 = delegate { };
+
+        /// <summary>
+        /// Second event.
+        /// </summary>
+        public event Action<int> SimpleEvent_1 = delegate { };
+
+        public SimpleData()
+        {
+
+        }
+
+        /// <summary>
+        /// First counter call events.
+        /// </summary>
+        public void Counter_0()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine($"Counter_0 Event happened. Data = {i}.");
+                    SimpleEvent_0();
+                    Thread.Sleep(150);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Second counter call events.
+        /// </summary>
+        public void Counter_1()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine($"Counter_1 Event happened {i}");
+                    SimpleEvent_1(i);
+                    Thread.Sleep(150);
+                }
+            }
+        }
+    }
+}
