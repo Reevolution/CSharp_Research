b0d154628fafc7f61ecac336f2edb9e72ba1d0f8
DESKTOP-LH38R6M\reevo <x5reunion@yandex.ru>
2017-07-22 14:03:46 +0300

    Added project read write binary data.

---
+++ IO/BinaryIO_Research/Simple_BinaryReadWrite/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_BinaryReadWrite
+{
+    // GitHub repo.
+    // https://github.com/Reevolution/CSharp_Research
+
+    /// <summary>
+    /// Simple example read write binary data file.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write simple data string to file.
+            using (BinaryWriter bw = new BinaryWriter(File.OpenWrite(@"D:\SimpleData.dat")))
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    bw.Write($"Simple string {i}");
+                }
+            }
+
+            // Read simple data string from file.
+            using (BinaryReader br = new BinaryReader(File.OpenRead(@"D:\SimpleData.dat")))
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    string data = br.ReadString();
+                    Console.WriteLine(data);
+                }
+            };
+        }
+    }
+}
